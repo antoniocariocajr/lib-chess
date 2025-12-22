859775b8caa8454658ca6c9fd6d2ba3d361b1df5
antoniocariocajr <antoniocariocajr@gmail.com>
2025-12-22 10:16:27 -0300

    Initial commit

---
+++ src/test/java/com/bill/chess/ChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
